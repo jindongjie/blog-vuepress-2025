80479e8ce16dafc989ada1970ce59ef73ba4cfa6
LoongArum <1@jindongjie.cn>
2025-02-10 15:31:14 +0800

    Refactor styles: migrate from CSS to SCSS, update client configuration, and enhance theme colors

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,9 +1,9 @@
-import { defineClientConfig } from 'vuepress/client'
-import RepoCard from 'vuepress-theme-plume/features/RepoCard.vue'
-import './styles/index.css'
+import { defineClientConfig } from "vuepress/client";
+import RepoCard from "vuepress-theme-plume/features/RepoCard.vue";
+import "./styles/index.scss";
 
 export default defineClientConfig({
   enhance({ app }) {
-    app.component('RepoCard', RepoCard)
+    app.component("RepoCard", RepoCard);
   },
-})
+});
